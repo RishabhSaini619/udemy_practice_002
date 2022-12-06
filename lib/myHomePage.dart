b3f1eeecb097a3d2d5060d9dd60074031f3b8c38
rishabhsaini619 <rishabhsaini619@gmail.com>
2022-12-06 16:42:26 +0530

    content based on orientation

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -56,6 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final appBarRow = AppBar(
       title: Text(
         'Expense Planner',
@@ -72,44 +74,54 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ],
     );
+    final trxList = Container(
+        height: (MediaQuery.of(context).size.height -
+                appBarRow.preferredSize.height) *
+            0.7,
+        child:
+            TransactionsList(userTransactionsList, deleteSelectedTransaction));
     return Scaffold(
-      // backgroundColor: Theme.of(context).accentColorBrightness,
       appBar: appBarRow,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Show Chart',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Switch(
-                  activeColor: Theme.of(context).colorScheme.secondary,
-                  value: showChart,
-                  onChanged: (value) {
-                    setState(() {
-                      showChart = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            showChart
-                ? Container(
-                    height: (MediaQuery.of(context).size.height -
-                            appBarRow.preferredSize.height) *
-                        0.7,
-                    child: Chart(recentTrx),
-                  )
-                : Container(
-                    height: (MediaQuery.of(context).size.height -
-                            appBarRow.preferredSize.height) *
-                        0.7,
-                    child: TransactionsList(
-                        userTransactionsList, deleteSelectedTransaction)),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Show Chart',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Switch(
+                    activeColor: Theme.of(context).colorScheme.secondary,
+                    value: showChart,
+                    onChanged: (value) {
+                      setState(() {
+                        showChart = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (isLandscape)
+              showChart
+                  ? Container(
+                      height: (MediaQuery.of(context).size.height -
+                              appBarRow.preferredSize.height) *
+                          0.7,
+                      child: Chart(recentTrx),
+                    )
+                  : trxList,
+            if (!isLandscape)
+              Container(
+                height: (MediaQuery.of(context).size.height -
+                    appBarRow.preferredSize.height) *
+                    0.3,
+                child: Chart(recentTrx),
+              ),
+            if (!isLandscape) trxList,
           ],
         ),
       ),
